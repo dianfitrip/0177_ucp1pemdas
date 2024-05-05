d777806c0feb21ed7040cd6ce381fbac8b503d1a
dianfitrip <dianfitri683@gmail.com>
2024-05-05 18:40:44 +0700

    Add project files.

---
+++ UCP1PemDas/UCP1PemDas.cpp
@@ -0,0 +1,82 @@
+// 1. a. float panjang, lebar  (MENGGUNAKAN FLOAT KARENA BISA SAJA NILAI ATAU HASIL DARI COMPUTE NYA ADA KOMA NYA)
+//    b. string NIM  (MENGGUNAKAN STRING KARENA NILAINYA TETAP)
+//    c. int umur    (MENGGUNAKAN INT KARENA NILAINYA BERUPA BILANGAN BULAT)
+
+//2. prosedur: void inputdata(){ 
+//					cout << " masukan panjangnya = "; 
+//					cin >> panjang;
+//				    cout << " masukan lebarnya = "; 
+//					cin >> lebar;
+//					}   PROSEDUR INI DIGUNAKAN UNTUK MENGINPUT DATA PANJANG DAN LEBAR
+//	fungsi: int fungsiLuas(int p, int l)
+//          {
+//      	return p * l;
+//          }  FUNGSI INI DIGUNAKAN UNTUK MENGHITUNG LUAS PERSEGI PANJANG
+
+// 3. Contoh Looping : cout << "Luas Persegi Panjang : " << hitungluas() << endl;
+//		looping ini digunakan untuk menampilkan hasil dari luas persegi panjang.
+//
+//
+//
+
+//4. do
+//   {
+//	 cout << "Perulangan Do...While" << endl;
+//	 x = rand() % 10;
+
+//	 cout << "Nilai X : " << x << endl;
+//	 } while (x > 4);
+// CONDITIONAL STATEMENT INI DIGUNAKAN KETIKA SELAMA ANGKA YANG KELUAR LEBIH BESAR DARI 4 MAKA AKAN TERUS BERULANG.
+
+
+//5. 
+//   struct Mahasiswa
+//	 {
+//	 string nim;
+//   string nama;
+//   string alamat;
+//   string umur;
+//   };    INI ADALAH STRUCT MAHASISWA, DIMANA DALAM STRUCT INI TERDIRI DARI NIM, NAMA, ALAMAT DAN UMUR YANG DIMILIKI MAHASISWA.
+
+
+#include <iostream>
+using namespace std;
+
+int DataPolusiUdara()
+{
+	string kota;
+	int jumlahpolusi;
+	int sensor[3];
+
+}
+
+int main() {
+	DataPolusiUdara;
+
+	cout << "nama kota : ";
+	cin >> kota;
+	cout << " jumlah polusinya : ";
+
+
+
+		int i;
+		int sensor[3];
+
+		for (i = 75; i > 75; i++) {
+			cout << i << "status nya : " << endl;
+		}
+
+		cout << "jumlah polusi : " << i << endl;
+
+		
+		for (i = 0; i < 3; i++) {
+			cout << "sensor ke-";
+			cin >> sensor;
+		}
+		return 0;
+	}
+
+
+
+
+
